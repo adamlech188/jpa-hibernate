42b09dc4b1ef15a3665cc6ea3041fb8d5bf82154
rhitgal1 <rhitgal1@comporium.com>
2015-03-30 11:28:26 -0400

    Sessions

---
+++ hibernate-jpa/src/main/java/com/hibernate/jpa/main/HelloWorld.java
@@ -0,0 +1,56 @@
+package com.hibernate.jpa.main;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.jpa.domain.Message;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld 
+{
+    public static void main( String[] args )
+    {
+        // First unit of work 
+    	
+    	Configuration configuration = new Configuration().configure(); 
+    	StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()); 
+    	SessionFactory factory = configuration.buildSessionFactory(builder.build());
+    	
+    	Session session = factory.openSession();
+    	
+    	Transaction transaction = session.beginTransaction(); 
+    	Message message = new Message("Hello World!");
+    	Long msgId = (Long)session.save(message); 
+    	
+    	transaction.commit();
+    	session.close(); 
+    	
+    	//Second unit of work 
+    	Session newSession = factory.openSession(); 
+    	
+    	Transaction newTransaction = newSession.beginTransaction(); 
+    	
+    	List<Message> messages = (List<Message>)newSession.createQuery("from Message m order by m.text asc").list();
+    	
+    	System.out.println(messages.size() +  " message(s) found: "); 
+    	
+    	for(Iterator<Message> iter = messages.iterator(); iter.hasNext();) {
+    		Message loadedMsg = iter.next(); 
+    		System.out.println(loadedMsg.getText()); 
+    	}
+    	
+    	newTransaction.commit(); 
+    	newSession.close(); 
+    	
+    	
+    }
+}
